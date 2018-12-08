3952e62f41d31414e872ff45ca6593b2194559d2
Chema Ajenjo <chema.ajenjo@gmail.com>
2018-12-08 19:10:24 +0100

    Initial commit

---
+++ src/main/java/com/littlebackupbot/LittleBackupBotApplication.java
@@ -0,0 +1,12 @@
+package com.littlebackupbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleBackupBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleBackupBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/littlebackupbot/LittleBackupBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.littlebackupbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LittleBackupBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
